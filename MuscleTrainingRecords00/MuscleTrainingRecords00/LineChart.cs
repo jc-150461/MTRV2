e646996fccfe5e5caa18bb347d5b58baa260537d
燎 石橋 <150461@jc-21.jp>
2018-02-04 17:29:50 +0900

    d

--- MuscleTrainingRecords00/MuscleTrainingRecords00/LineChart.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/LineChart.cs
@@ -101,12 +101,14 @@ namespace MuscleTrainingRecords00
                 Title = "体重(kg)　体脂肪率(%)",
                 IsZoomEnabled = false,
                 Position = AxisPosition.Left,
-                Maximum = 100,
-                Minimum = 10,
+                Maximum = 150,
+                Minimum = 50,
                 MajorStep = 10,
                 MajorGridlineStyle = LineStyle.Solid,
                 MinorGridlineStyle = LineStyle.Dot,
-               
+                ExtraGridlines = new double[] { 1, 2, 3, 8, 9, 10 },
+                ExtraGridlineThickness = 3,
+                ExtraGridlineColor = OxyColors.SkyBlue,
 
 
 
@@ -137,8 +139,8 @@ namespace MuscleTrainingRecords00
 
 
 
-            var startDate = DateTime.Today.AddDays(-1);
-            var endDate = DateTime.Today.AddDays(+2);
+            var startDate = DateTime.Today;
+            var endDate = DateTime.Today.AddDays(+7);
 
             var minValue = DateTimeAxis.ToDouble(startDate);
             var maxValue = DateTimeAxis.ToDouble(endDate);
